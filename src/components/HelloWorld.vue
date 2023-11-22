d98b41acfa7ce73154c535cda7063aec5a94bc69
HellenCuahutle <107901049+HellenCuahutle@users.noreply.github.com>
2023-11-22 13:22:15 -0600

    Add files via upload
    "Estado de un botón, cambio mediante evento"

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>
+      For a guide and recipes on how to configure / customize this project,<br>
+      check out the
+      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+    </p>
+    <button class="btn" :class="{ 'btn-primary': status, 'btn-secondary': !status }" @click="toggleState">{{ status ?
+      'Encendido' : 'Apagado' }}</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String,
+
+  },
+
+  data: function () {
+
+    return {
+      status: false
+    };
+
+  },
+
+  methods: {
+    toggleState: function () {
+      this.status = !this.status;
+    }
+  }
+
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
